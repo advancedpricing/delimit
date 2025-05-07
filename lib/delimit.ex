a975b0caa57b755d58f1e48531ec507a4b6fd2f7
Jeremy Cowgar <jeremy@cowgar.com>
2025-05-07 10:08:17 -0400

    initial import

---
+++ lib/delimit.ex
@@ -0,0 +1,18 @@
+defmodule Delimit do
+  @moduledoc """
+  Documentation for `Delimit`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Delimit.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/delimit_test.exs
@@ -0,0 +1,8 @@
+defmodule DelimitTest do
+  use ExUnit.Case
+  doctest Delimit
+
+  test "greets the world" do
+    assert Delimit.hello() == :world
+  end
+end
